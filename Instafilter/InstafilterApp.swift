b4120b1e739e27c9dcd78472ad0f58b74083830c
Javier Alaves <jayalaves@gmail.com>
2023-07-30 09:05:47 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Javier Alaves on 30/7/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
